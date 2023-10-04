bc7dec8d1ba539da21dba09dec3bd4de6b39c047
Andrea Agili <agea@drwolf.it>
2023-10-04 12:32:12 +0200

    :passport_control: auth

--- src/main/java/it/drwolf/resources/BookResource.java
+++ src/main/java/it/drwolf/resources/BookResource.java
@@ -1,24 +1,34 @@
 package it.drwolf.resources;
 
+import io.quarkus.security.Authenticated;
 import it.drwolf.model.dtos.BookDTO;
 import it.drwolf.model.entities.Book;
 import it.drwolf.repositories.BookRepository;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
 import java.util.List;
 
 @Path("/book")
+@Authenticated
 public class BookResource {
 
 	@Inject
 	BookRepository bookRepository;
 
+	@Inject
+	JsonWebToken jwt;
+
 
 	@GET
+	@RolesAllowed("corso")
 	public List<Book> all(){
 		return bookRepository.listAll();
 	}
